53878f610cb206a3761fee5dd6f5e5d10bfc9189
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-04 22:58:19 +0530

    Copy Array in reverse order

---
+++ Array/Copy_Reverse_array.cpp
@@ -0,0 +1,25 @@
+// Take 10 integer inputs from user and store them in an array.
+// Now, copy all the elements in another array but in reverse order
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10],brr[10];
+    for(int i=0;i<10;i++){
+        cin >> arr[i];
+    }
+
+    int j = 0;
+    for (int i=9;i>=0;i--){
+        brr[i] = arr[j];
+        j++;
+    }
+
+    for(int i=0;i<10;i++){
+        cout << brr[i] << " ";
+    }
+
+    return 0;
+}
