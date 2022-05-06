51343505e9704495806992b79cea61ebb0680d55
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-06 10:35:04 +0530

    Update Sum_Product.cpp

--- Array/Sum_Product.cpp
+++ Array/Sum_Product.cpp
@@ -14,14 +14,14 @@ int main()
         cin >> arr[i];
     }
     int sum = 0;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < n; i++)
     {
         sum = sum + arr[i];
     }
 
     cout << "Sum: " << sum << endl;
     int product = 1;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < n; i++)
     {
         product = product * arr[i];
     }
