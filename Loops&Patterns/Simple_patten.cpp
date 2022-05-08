32424566454dfb8f11ced24c33cc486a709fbfa2
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-08 14:39:31 +0530

    Programe To print simple pattern

---
+++ Loops&Patterns/Simple_patten.cpp
@@ -0,0 +1,26 @@
+// Write a C++ program to print
+// *
+// **
+// ***
+// ****
+// on screen.(For n=4)
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Number of Rows: " << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
