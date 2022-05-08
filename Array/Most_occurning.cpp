47638dcb6870a7b245e12c2490c20e1f24310296
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-08 19:26:52 +0530

    Programe to find most frequent element

--- Array/Most_occurning.cpp
+++ Array/Most_occurning.cpp
@@ -1,30 +1,41 @@
-// Write a C++ program to find the most occurring element in an array of integers
+
+// Most Frequent in array
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int n;
-    cout << "Enter Size of array: " << endl;
+    cout << "Enter Size of Array: " << endl;
     cin >> n;
+
     int arr[n];
     cout << "Enter Elements: " << endl;
-    for (int i = 0; i < n; i++)
-    {
+    for(int i=0; i<n;i++){
         cin >> arr[i];
     }
-    int most;
-    cout << "Most Occuring Element is : ";
-    for (int i = 0; i < n; i++)
+
+    int mf = -1;            // Initializing Most Frequent;
+    int maxCount = 0;       // For counting Whch element occurs most times
+
+    for (int i = 0; i < 5; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        int count = 0;      // Counting Occurence of each element
+        for (int j = i + 1; j < 5; j++)
         {
             if (arr[i] == arr[j])
             {
-                most = arr[i];
+                count++;
             }
         }
+        if (count > maxCount)
+        {
+            maxCount = count;
+            mf = arr[i];
+        }
     }
-    cout << most;
+    cout << "Most Occuring Element is : " << mf << endl;
+
     return 0;
 }
