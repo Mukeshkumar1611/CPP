29dda09307767d9433af1cd76839525d92754505
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-09 13:42:42 +0530

    Pattern of numbers in Increasing order

---
+++ Loops&Patterns/Numbers_pattern.cpp
@@ -0,0 +1,26 @@
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15 (For n = 5)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Number of Rows: " << endl;
+    cin >> n;
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
