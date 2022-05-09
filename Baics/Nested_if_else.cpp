2b1c24dc4298c440b88ca0a653c18d17cc458608
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-09 13:37:35 +0530

    Nested if

---
+++ Baics/Nested_if_else.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int savings;
+    cin >> savings;
+    if (savings > 5000)
+    {
+        if (savings > 10000)
+        {
+            cout << "Roadtrip with Neha/n";
+        }
+        else
+        {
+            cout << "shopping with neha/n";
+        }
+    }
+    else if (savings > 2000)
+    {
+        cout << "Go woith rashmi\n";
+    }
+    else
+    {
+        cout << "Go with friends\n";
+    }
+    return 0;
+}
