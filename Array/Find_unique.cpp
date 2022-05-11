22c1db0aa1db1aa5206e877e53109e1f9ac44967
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-11 14:45:56 +0530

    find element in array which occured only once

---
+++ Array/Find_unique.cpp
@@ -0,0 +1,34 @@
+// FIND UNIQUE
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Size of Array: " << endl;
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter Elements: " << endl;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        if (count == 1)
+        {
+            cout <<"Unique Element in array is : " <<arr[i];
+        }
+    }
+
+    return 0;
+}
