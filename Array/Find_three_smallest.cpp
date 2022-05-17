6c9da53e513f2845a9e240263dd2a20885320c9f
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-17 10:30:28 +0530

    find the smallest three elements in an array.

---
+++ Array/Find_three_smallest.cpp
@@ -0,0 +1,47 @@
+//  Write a C++ program to find the smallest three elements in an array.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Size Of Array: " << endl;
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter Elemets : " << endl;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int smallest = INT_MAX;
+    int second_smallest = INT_MAX;
+    int third_smallest = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            third_smallest = second_smallest;
+            second_smallest = smallest;
+            smallest = arr[i];
+        }
+
+        else if (arr[i] < second_smallest)
+        {
+            third_smallest = second_smallest;
+            second_smallest = arr[i];
+        }
+
+        else if (arr[i] < third_smallest)
+        {
+            third_smallest = arr[i];
+        }
+    }
+
+    cout << "Smallest: " << smallest << endl;
+    cout << "Second Smallest: " << second_smallest << endl;
+    cout << "Third Smallest: " << third_smallest << endl;
+
+    return 0;
+}
