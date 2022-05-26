6be5c241318588360f5be47bfee8881953e0ed90
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-26 09:10:12 +0530

    Create wave_Print.cpp

---
+++ 2DArrys/wave_Print.cpp
@@ -0,0 +1,42 @@
+// Wave Print
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, colums;
+    cout << "Enter Number of Rows: " << endl;
+    cin >> rows;
+    cout << "Enter Number of Columns: " << endl;
+    cin >> colums;
+
+    int arr[rows][colums];
+    cout << "Enter Elements: " << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int j = 0; j < colums; j++)
+    {
+        if (j % 2 == 0)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            for (int i = colums - 1; i >= 0; i--)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+    }
+
+    return 0;
+}
