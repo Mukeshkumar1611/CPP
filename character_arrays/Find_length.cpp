f3cb662b4153c4e44058b155cba8fe8f472246e4
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-26 09:17:36 +0530

    To find Lenhgth Of a Character Array

---
+++ character_arrays/Find_length.cpp
@@ -0,0 +1,25 @@
+// To find Lenhgth Of a Character Array
+
+#include <iostream>
+using namespace std;
+
+int len(char arr[])
+{
+    int count = 0;
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    char arr[100];
+    cout << "Enter String: " << endl;
+    cin.getline(arr, 100);
+
+    cout << "Length Of String is : " << len(arr) << endl;
+
+    return 0;
+}
