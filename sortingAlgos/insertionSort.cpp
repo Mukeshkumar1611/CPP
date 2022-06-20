ecaa8887f6cd70d60bb0e2daeeb907fdb6ed7347
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-06-20 13:14:23 +0530

    Create insertionSort.cpp

---
+++ sortingAlgos/insertionSort.cpp
@@ -0,0 +1,40 @@
+
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int *arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    insertionSort(arr, n);
+    cout << "Your sorted array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
