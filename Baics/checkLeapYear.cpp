eb920eed67457866b5bd60df0206cb9d63b8ab78
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-06-30 17:28:42 +0530

    Create checkLeapYear.cpp

---
+++ Baics/checkLeapYear.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout << "Enter Year You want to check: " << endl;
+    cin >> year;
+
+    if (year % 100 == 0)
+    {
+        if (year % 400 == 0)
+        {
+            cout << "Leap Year" << endl;
+        }
+        else
+        {
+            cout << "Not A Leap Year" << endl;
+        }
+    }
+
+    else
+    {
+        if (year % 4 == 0)
+        {
+            cout << "Leap Year" << endl;
+        }
+        else
+        {
+            cout << "Not A Leap Year" << endl;
+        }
+    }
+
+    return 0;
+}
