29c86b399c2d4da396d7ca732600d92b39ce49de
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-09-10 13:38:03 +0530

    Sum of nodes in generic tree.

---
+++ GenericTrees/sumNodes.cpp
@@ -0,0 +1,71 @@
+// Sum of nodes in generic tree.
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<int> *> children;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
+
+TreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter root data " << endl;
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+    queue<TreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        TreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        int numOfChild;
+        cout << "Enter number of children of " << front->data << endl;
+        cin >> numOfChild;
+        for (int i = 0; i < numOfChild; i++)
+        {
+            int childData;
+            cout << "Enter data of " << i << "th child of " << front->data << endl;
+            cin >> childData;
+            TreeNode<int> *child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+int sumOfNodes(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int sum = root->data;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        sum += sumOfNodes(root->children[i]);
+    }
+    return sum;
+}
+
+int main()
+{
+    TreeNode<int> *root = takeInput();
+    cout << "Sum of nodes : " << sumOfNodes(root) << endl;
+    return 0;
+}
