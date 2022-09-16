a81b98090edf61f37dec56eeb93fd26878e2b9c2
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-09-16 15:01:53 +0530

    Postorder Traversal

---
+++ BinaryTrees/postorder.cpp
@@ -0,0 +1,80 @@
+// Postorder Traversal
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+BinaryTreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+
+        int leftChild;
+        cout << "Enter left child of " << front->data << endl;
+        cin >> leftChild;
+        if (leftChild != -1)
+        {
+            BinaryTreeNode<int> *leftC = new BinaryTreeNode<int>(leftChild);
+            front->left = leftC;
+            pendingNodes.push(leftC);
+        }
+
+        int rightChild;
+        cout << "Enter right child of " << front->data << endl;
+        cin >> rightChild;
+        if (rightChild != -1)
+        {
+            BinaryTreeNode<int> *rightC = new BinaryTreeNode<int>(rightChild);
+            front->right = rightC;
+            pendingNodes.push(rightC);
+        }
+    }
+    return root;
+}
+
+void postOrder(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    BinaryTreeNode<int> *root = takeInput();
+    postOrder(root);
+}
