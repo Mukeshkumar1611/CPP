f9b6894cf8022e392d679be923455dded5bfd46a
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-10-28 13:46:27 +0530

    Find count of subsets of array that sum up to K.

---
+++ Backtracking/subsetSum.cpp
@@ -0,0 +1,44 @@
+// Subset sum
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void helper(int *arr, int n, int k, int &count, int sum)
+{
+    if(n == 0)
+    {
+        if(sum == k)
+        {
+            count++;
+        }
+        return;
+    }
+    helper(arr + 1, n - 1, k, count, sum);
+    helper(arr + 1, n - 1, k, count, sum + arr[0]);
+}
+
+int getCount(int *arr, int n, int k)
+{
+    int count = 0;
+    int sum = 0;
+    helper(arr, n, k, count, sum);
+    return count;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        cout << getCount(arr, n , k) << endl;
+    }
+}
+
