97994f2964a9ec8b8fda13666b234935b645629f
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2023-06-19 16:05:11 +0530

    Update mergeSort.cpp

--- sortingAlgos/mergeSort.cpp
+++ sortingAlgos/mergeSort.cpp
@@ -1,86 +1,67 @@
-// Merge Sort
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void merge(int *arr, int start, int end)
+void merge(int *arr, int start, int mid, int end)
 {
-    int mid = (start + end) / 2;
-    int size = (end - start) + 1;
-    int i = start;
-    int j = mid + 1;
-    int k = 0;
-    int *output = new int[size];
-
-    while (i <= mid && j <= end)
+    int left = start;
+    int right = mid + 1;
+    vector<int> temp;
+    
+    while(left <= mid && right <= end)
     {
-        if (arr[i] < arr[j])
+        if(arr[left] <= arr[right])
         {
-            output[k] = arr[i];
-            i++;
-            k++;
+            temp.push_back(arr[left]);
+            left++;
         }
         else
         {
-            output[k] = arr[j];
-            j++;
-            k++;
+            temp.push_back(arr[right]);
+            right++;
         }
     }
-
-    while (i <= mid)
+    while(left <= mid)
     {
-        output[k] = arr[i];
-        i++;
-        k++;
+        temp.push_back(arr[left]);
+        left++;
     }
-
-    while (j <= end)
+    while(right <= end)
     {
-        output[k] = arr[j];
-        j++;
-        k++;
+        temp.push_back(arr[right]);
+        right++;
     }
-    int m = 0;
-    for (int i = start; i <= end; i++)
+    int k = 0;
+    for(int i = start; i <= end; i++)
     {
-        arr[i] = output[m];
-        m++;
+        arr[i] = temp[k++];
     }
-    delete[] output;
 }
 
-void mergeSort(int *arr, int start, int end)
+void merge_sort(int *arr, int start, int end)
 {
-
-    if (start >= end)
+    if(start >= end)
     {
         return;
     }
-    int mid = (start + end) / 2;
-    mergeSort(arr, start, mid);
-    mergeSort(arr, mid + 1, end);
-    merge(arr, start, end);
+    int mid = (start + end)/ 2;
+    merge_sort(arr, start, mid);
+    merge_sort(arr, mid + 1, end);
+    merge(arr, start, mid, end);
 }
+
 int main()
 {
     int n;
-    cout << "Enter size of array : ";
     cin >> n;
-
     int arr[n];
-    cout << "Enter elements : ";
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    int start = 0;
-    int end = n - 1;
-    mergeSort(arr, start, end);
-    cout << "Output : ";
-    for (int i = 0; i < n; i++)
+    merge_sort(arr, 0, n-1);
+    for(int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        cout << arr[i] << ' ';
     }
     return 0;
 }
