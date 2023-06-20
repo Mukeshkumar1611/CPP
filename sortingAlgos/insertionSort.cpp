b8e2c991395136c7c7851bfddf9c613bf4e97847
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2023-06-20 09:46:42 +0530

    Update insertionSort.cpp

--- sortingAlgos/insertionSort.cpp
+++ sortingAlgos/insertionSort.cpp
@@ -1,40 +1,31 @@
-// Insertion Sort
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void insertionSort(int *arr, int n)
+void insertion_sort(int *arr, int n)
 {
-    for (int i = 1; i < n; i++)
+    for(int i = 1; i < n - 1; i++)
     {
-        int temp = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > temp)
+        int j = i;
+        while(j > 0 and arr[j]< arr[j-1])
         {
-            arr[j + 1] = arr[j];
+            swap(arr[j], arr[j-1]);
             j--;
         }
-        arr[j + 1] = temp;
     }
 }
 
-int main()
-{
+int main() {
     int n;
-    cout << "Enter size of array : ";
     cin >> n;
-
     int arr[n];
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    insertionSort(arr, n);
-    cout << "Your sorted array : ";
-    for (int i = 0; i < n; i++)
+    insertion_sort(arr, n);
+    for(int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        cout << arr[i] << ' '; 
     }
     return 0;
 }
